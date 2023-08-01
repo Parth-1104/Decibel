e22675e6bc78833745fd78cd185bb3768a070c92
Parth-1104 <141064865+Parth-1104@users.noreply.github.com>
2023-08-01 20:11:35 +0530

    updated the code

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("welcome to Decibel");
+let songs= [
+    
+        {songName:"Cheque",filePath: "song/Cheques.mp3",coverPath:"cover/shubh.jpg"},
+        {songName:"Cheque",filePath: "song/Cheques.mp3",coverPath:"cover/shubh.jpg"},
+        {songName:"Cheque",filePath: "song/Cheques.mp3",coverPath:"cover/shubh.jpg"},
+       {songName:"Cheque",filePath: "song/Cheques.mp3",coverPath:"cover/shubh.jpg"},
+       {songName:"Cheque",filePath: "song/Cheques.mp3",coverPath:"cover/shubh.jpg"},
+       {songName:"Cheque",filePath :"song/Cheques.mp3",coverPath:"cover/shubh.jpg"},
+       {songName:"Cheque",filePath :"song/Cheques.mp3",coverPath:"cover/shubh.jpg"},
+       {songName:"Cheque",filePath :"song/Cheques.mp3",coverPath:"cover/shubh.jpg"},
+       {songName:"Cheque",filePath :"song/Cheques.mp3",coverPath:"cover/shubh.jpg"},
+    
+]
+let audioElement=new Audio('Cheques.mp3');
